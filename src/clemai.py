227d58d7070b0b07c527dee615046f4c0d798d2d
ThomasOMara <tpomara@gmail.com>
2024-09-10 21:38:14 +1000

    Remove outdated comments

--- src/clemai.py
+++ src/clemai.py
@@ -6,21 +6,22 @@ import mic_input
 
 def main():
 
-    # Todo: do we want to test the mic and speakers are working here?
+   # Todo: do we want to test the mic and speakers are working here?
 
     mic_input.get_mic_input()
 
+
     '''
-    while(True):
-        print("hello world")
+    Threads
 
-        # 1. Take input from mic and convert to text
+    1. mic_input_thread
+       - continuous monitor for audio and add buffer to queue
 
+    2. speech_to_text_thread
 
-        # 2. Run the sentiment analyser over the text input
+    3. sentiment_analysis_thread
 
-
-        # 3. Output the set text via speaker
+    4. speech_output_thread
 
     '''
 
@@ -34,19 +35,13 @@ NEXT
 Mic Input
 
 x. Try to get better performance from mic
-x. Code mic_input to take input from mic
-   - Maybe first save it down as a wav
-x. Setup the google service to translate the audio to text
-x. Link the mic_input to the google text api
-   - Maybe first save down as a text file
+
 ---
 
 Sentiment Analysis
 
 x. Code the sentiment analyser
-   - Develop list of words
-   - Run over last x seconds of text
-   - Test with actual speech to refine (look at transcripts)
+   - Download and run VADER
 
 ---
 
@@ -59,10 +54,7 @@ x. Code the speaker_output to output text when triggered
 
 THOUGHTS
 
-x. Do we need to download API keys or something?
-   - How would be "hide" these from github - add to .gitignore?
-
-x. We might want to create a new cpu thread for the audio input
+x. Threading (see project doc)
 
 x. Make sure we turn off audio input whilst playing from speaker
 
--- src/mic_input.py
+++ src/mic_input.py
@@ -1,5 +1,6 @@
 '''
-Convert speech from mic into text stream
+Create stream of audio data captured from mic
+Convert audio data speech into text
 '''
 
 def get_mic_input():
@@ -17,138 +18,26 @@ def get_mic_input():
 
   model = Model(vosk_model_path)
   recogniser = KaldiRecognizer(model, 16000)
-  #recogniser.SetWords(True) # For detailed jason
-  word_array = []
 
-  p = pyaudio.PyAudio()
-  stream = p.open(format = pyaudio.paInt16, channels = 1, rate = 16000, input = True, frames_per_buffer = 8000)
+  audio_interface = pyaudio.PyAudio()
+  stream = audio_interface.open(format = pyaudio.paInt16, channels = 1, rate = 16000, input = True, frames_per_buffer = 8000)
   stream.start_stream()
 
   print("Listening... Ctrl+C to stop.")
-
-  num_processed_words = 0
 
   try:
     while True:
 
       data = stream.read(4000, exception_on_overflow = False)
 
-      '''
-      if recognizer.AcceptWaveform(data):
-        # Full result obtained after sentence completion
-        result = json.loads(recognizer.Result())
-        if 'text' in result and result['text'].strip():
-          recognized_text = result['text']
-          print(f"Recognized sentence: {recognized_text}")
-
-
-      else:
-        # Partial result obtained word by word
-        partial_result = json.loads(recognizer.PartialResult())
-        if 'partial' in partial_result and partial_result['partial'].strip():
-          word = partial_result['partial']
-          word_array.append(word)
-          print(f"Recognized word: {word}")
-      '''
-
-      '''
-      # Trying to get partial words (word-by-word)
-
-      if recognizer.AcceptWaveform(data):
-        sentence_dict = json.loads(recognizer.Result())
-        results_sentence_len = len(sentence_dict['result'])
-        for i in range(num_processed_words, results_sentence_len):
-          word_array.append(sentence_dict['result'][i] )
-
-        # Reset
-        num_processed_words = 0
-        sentence_dict = ""
-        recognizer = KaldiRecognizer(model, 16000)
-        recognizer.SetWords(True)
-
-      else:
-
-        partial_result = json.loads(recognizer.PartialResult())
-
-        print(partial_result)
-        #print(len(partial_result))
-
-        if 'partial' in partial_result and partial_result['partial'].strip():
-          word = partial_result['partial']
-
-          print(partial_result)
-          #print("word" + word)
-
-          word_array.append(word)
-          #print(f"Recognized word: {word}")
-      '''
-
-      # Just get the whole sentence
-
       if recogniser.AcceptWaveform(data):
         sentence_dict = json.loads(recogniser.Result())
         print(sentence_dict['text'])
 
-        # Reset
-        num_processed_words = 0
-        sentence_dict = ""
-        recogniser = KaldiRecognizer(model, 16000)
-        #recogniser.SetWords(True)
-
   except KeyboardInterrupt:
     print("\nProgram interrupted with Ctrl+C. Exiting...")
-    print(f"Captured words: {word_array}")
 
   finally:
     stream.stop_stream()
     stream.close()
-    p.terminate()
-
-
-'''
-# Save mic input into wav file
-def get_mic_input():
-
-  import pyaudio
-  import wave
-
-  buffer_size = 1024
-  audio_format = pyaudio.paInt16
-  num_channels = 1
-  sample_rate = 8000 # kHz sampling rate
-  record_length = 5 # recording time in seconds
-  output_filename = "output.wav"
-  #mic_device_index = 3
-
-  pyaudio_instance = pyaudio.PyAudio()
-
-  audio_input_stream = pyaudio_instance.open(
-                      format = audio_format,
-                      channels = num_channels,
-                      rate = sample_rate,
-                      input = True,
-                      frames_per_buffer = buffer_size,
-                      #input_device_index = mic_device_index # Including throws error
-                      )
-
-  print("Recording in progress")
-
-  stream_frames = []
-
-  for _ in range(0, int(sample_rate / buffer_size * record_length)):
-    data = audio_input_stream.read(buffer_size)
-    stream_frames.append(data)
-
-  print("Recording complete")
-
-  audio_input_stream.stop_stream()
-  audio_input_stream.close()
-  pyaudio_instance.terminate()
-
-  wave_file = wave.open(output_filename, 'wb')
-  wave_file.setnchannels(num_channels)
-  wave_file.setsampwidth(pyaudio_instance.get_sample_size(audio_format))
-  wave_file.setframerate(sample_rate)
-  wave_file.writeframes(b''.join(stream_frames))
-  wave_file.close()
-'''
+    audio_interface.terminate()
